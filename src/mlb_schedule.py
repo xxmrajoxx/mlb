31f4941e8414dacfd9690a89fa0ad10cc677e666
xxmrajoxx <andrew.chang2@live.com.au>
2026-03-17 19:52:58 +1100

    updated schedule table

---
+++ src/mlb_schedule.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import requests
+import json
+import logging
+
+from sql.sql_loader import load_dataframe
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info("Starting MLB schedule extraction")
+
+url = "https://statsapi.mlb.com/api/v1/schedule?sportId=1&season=2026"
+response = requests.get(url)
+response.raise_for_status()
+
+data = response.json()
+
+full_schedule = pd.json_normalize(
+    data["dates"],
+    record_path="games"
+) 
+
+full_schedule.columns = full_schedule.columns.str.replace(".", "_", regex=False)
+load_dataframe(full_schedule, "mlb_schedule", if_exists="replace")
+
+
+
+# print(data.keys())
+# print(data["dates"][0].keys())
+# print(data["dates"][0]["games"][0].keys())
+# print(json.dumps(data["dates"][0]["games"][0], indent=2))
+# df = pd.json_normalize(data["dates"], record_path="games")
+# print (df.columns)
+# print (df.head())
