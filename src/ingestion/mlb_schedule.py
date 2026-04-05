1f9176fcfdefbd0bd9222bccc727e30c17e99021
xxmrajoxx <andrew.chang2@live.com.au>
2026-04-05 14:10:08 +1000

    updated mlb_schedule to be function and created mlb_schedule to show standings

--- src/ingestion/mlb_schedule.py
+++ src/ingestion/mlb_schedule.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 import json
 import logging
+from datetime import datetime
 
 from sql.sql_loader import load_dataframe
 
@@ -9,19 +10,41 @@ logging.basicConfig(level=logging.INFO)
 
 logging.info("Starting MLB schedule extraction")
 
-url = "https://statsapi.mlb.com/api/v1/schedule?sportId=1&season=2026"
-response = requests.get(url)
-response.raise_for_status()
+def mlb_schedule()->pd.DataFrame:
+     
+    url = "https://statsapi.mlb.com/api/v1/schedule?sportId=1&season=2026"
+    
+    try: 
+        response = requests.get(url, timeout=30)
+        response.raise_for_status()
+        data = response.json()
 
-data = response.json()
+    except requests.exception.requestException as e:
+        logging.error(f"Error: {e}")
+        return pd.DataFrame()
 
-full_schedule = pd.json_normalize(
-    data["dates"],
-    record_path="games"
-) 
+    full_schedule = pd.json_normalize(
+        data["dates"],
+        record_path="games"
+    ) 
 
-full_schedule.columns = full_schedule.columns.str.replace(".", "_", regex=False)
-load_dataframe(full_schedule, "mlb_schedule", if_exists="replace")
+    full_schedule.columns = full_schedule.columns.str.replace(".", "_", regex=False)
+
+    if full_schedule.empty:
+        logging.warning("No schedule data returned")
+        return pd.DataFrame()
+
+    load_dataframe(full_schedule, "mlb_schedule", if_exists="replace")
+    logging.info(f"Loaded {len(full_schedule):,} rows into mlb_schedule")
+
+    return full_schedule
+
+if __name__=="__main__":
+    df = mlb_schedule()
+    
+
+
+
 
 
 # print(json.dumps(data, indent=2))
